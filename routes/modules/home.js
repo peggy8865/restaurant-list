a5c7465759f669d5e0a90463f2ed76fc9399c438
peggy8865 <peggy8865@gmail.com>
2022-11-20 07:53:39 +0800

    Add sort function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,17 +3,31 @@ const router = express.Router()
 
 const RestaurantModel = require('../../models/restaurant')
 
+function getSortOrder(sortOption) {
+  switch (sortOption) {
+    case 'AtoZ': return { name: 'asc' };
+    case 'ZtoA': return { name: 'desc' };
+    case 'category': return { category: 'asc' };
+    case 'location': return { location: 'asc' };
+    default: return { _id: 'asc' }
+  }
+}
+
 router.get('/', (req, res) => {
+  const sort = req.query.sort
   RestaurantModel.find()
     .lean()
+    .sort(getSortOrder(sort))
     .then(restaurants => res.render('index', { restaurants }))
     .catch(error => console.log(error))
 })
 
 router.get('/search', (req, res) => {
   const keyword = req.query.keyword
+  const sort = req.query.sort
   RestaurantModel.find()
     .lean()
+    .sort(getSortOrder(sort))
     .then(restaurants => {
       let restaurantsFiltered = restaurants.filter(item => {
         const nameMatched = item.name.toLowerCase().includes(keyword.trim().toLowerCase())
